dc2f5866a2449ebc53c46b719d4adcf921f585d3
Dkyros <dennisobala@gmail.com>
2018-01-12 19:03:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function logShout(string){
   var allcaps = string.toUpperCase()
   console.log(allcaps);
 }
+
+function logWhisper(string){
+  var all_lower = string.toLowerCase()
+  console.log(all_lower)
+}
